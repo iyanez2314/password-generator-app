25a360a0c678de374438d430b71e52194a8a28f1
Isaac Yanez <isaac231467@gmail.com>
2023-02-19 21:38:16 -0600

    finished with the functionality of the website need to add some styling

--- js/index.js
+++ js/index.js
@@ -1,44 +1,83 @@
+const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz".split("");
+const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+const NUMBERS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const SYMBOLS = ["!", "@", "#", "$", "%", "^", "&", "*"];
 const generatedPasswordElem = document.querySelector(".generated-password");
 const sliderElem = document.querySelector("#myRange");
 const sliderOutputElem = document.querySelector("#slider-output");
 const copyButtonElem = document.querySelector("#copy-btn");
-sliderElem.oninput = function () {
-  sliderOutputElem.innerHTML = this.value;
+const checkboxElements = document.querySelectorAll(".checkbox");
+const generateButtonElem = document.querySelector("#generate-btn");
+console.log(checkboxElements);
+let passowrdLength = sliderElem.value;
+
+const state = {
+  lowercase: false,
+  uppercase: false,
+  numbers: false,
+  symbols: false,
 };
 
-copyButtonElem.addEventListener("click", async () => {
+function updateState(checkbox) {
+  const name = checkbox.getAttribute("name");
+  const checked = checkbox.checked;
+  state[name] = checked;
+}
+/**
+ * Will Display the value of what ever the slider value is
+ */
+sliderElem.oninput = function () {
+  sliderOutputElem.innerHTML = this.value;
+  passowrdLength = this.value;
+};
+
+checkboxElements.forEach((checkbox) => {
+  checkbox.addEventListener("click", function () {
+    updateState(checkbox);
+  });
+});
+
+/**
+ * Will Give us the possibility to copy what ever is generated for the passowrd to the clipboard
+ */
+async function clipboard() {
   try {
     await navigator.clipboard.writeText(generatedPasswordElem.innerHTML);
     console.log("Content copied to clipboard");
   } catch (err) {
     console("Failed to copy: ", err);
   }
+}
+
+/**
+ *
+ * @param {*} checkboxes takes in a argument of the node list of checkbox arrays
+ * @returns the list of the checked items in the array
+ */
+
+function getCheckboxes(checkboxes) {
+  let checked = [];
+  for (let i = 0; i < checkboxes.length; i++) {
+    if (checkboxes[i].checked) {
+      checked.push(checkboxes[i].getAttribute("name"));
+    }
+  }
+  return checked;
+}
+
+copyButtonElem.addEventListener("click", clipboard);
+generateButtonElem.addEventListener("click", function () {
+  const checkedItems = getCheckboxes(checkboxElements);
+  if (checkedItems.length === 0) {
+    return;
+  }
+  while (true) {
+    try {
+      const newPassword = generatePassword(passowrdLength, state);
+      generatedPasswordElem.innerHTML = newPassword;
+      break;
+    } catch (err) {
+      console.error(err);
+    }
+  }
 });
-
-// const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz".split("");
-// const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-// const NUMBERS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-// const SYMBOLS = ["!", "@", "#", "$", "%", "^", "&", "*"];
-
-// const passwordLength = parseInt(
-//   window.prompt("How many characters would you like in your password?")
-// );
-// const lowerCase = window.confirm("Would you like to include lowecase letters?");
-// const upperCase = window.confirm(
-//   "Would you like to include uppercase letters?"
-// );
-// const specialChar = window.confirm(
-//   "Would you like to include special char letters?"
-// );
-// const symbols = window.confirm("Would you like to include symbols?");
-// const numbers = window.confirm("Would you like numbers?");
-
-// const password = generatePassword(
-//   passwordLength,
-//   lowerCase,
-//   upperCase,
-//   numbers,
-//   symbols
-// );
-
-// generatedPasswordElem.innerHTML = `${password}`;
--- js/utils/passwordGenerator.js
+++ js/utils/passwordGenerator.js
@@ -1,46 +1,33 @@
 /**
  *
- * @param {*} length - The length of the password we need to generate
- * @param {*} includeLower - Boolean value to include lower case characters
- * @param {*} includeUpper - Boolean value to include upper case characters
- * @param {*} includeNumbers - Boolean value to include numbers
- * @param {*} includeSymbols - Boolean value to include symbols
- * @returns the generated password
+ * @param {*} length The length of the password being passed in
+ * @param {*} state the state of the options
+ * @returns the password combination
  */
 
-function generatePassword(
-  length,
-  includeLower,
-  includeUpper,
-  includeNumbers,
-  includeSymbols
-) {
-  let password = "";
+function generatePassword(length, state) {
   let characters = "";
-
-  if (includeLower) {
-    characters += randomizeLower();
+  if (state.lowercase) {
+    characters += lowercaseLetters.join("");
   }
-  if (includeUpper) {
-    characters += randomizeUpper();
+  if (state.uppercase) {
+    characters += uppercaseLetters.join("");
   }
-  if (includeNumbers) {
-    characters += randomizeSpecialNum();
+  if (state.numbers) {
+    characters += NUMBERS.join("");
   }
-  if (includeSymbols) {
-    characters += randomizeSpecialChar();
+  if (state.symbols) {
+    characters += SYMBOLS.join("");
   }
-
   if (characters.length === 0) {
     throw new Error(
       "Invalid options: Please include at least one type of characters"
     );
   }
-
+  let password = "";
   for (let i = 0; i < length; i++) {
     const randomIndex = Math.floor(Math.random() * characters.length);
     password += characters[randomIndex];
   }
-
   return password;
 }
